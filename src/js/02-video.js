a7809a8c258ad708d760504ba15443bdec4ea7da
dburdin <dmitribevziuk@gmail.com>
2023-02-07 12:13:52 +0200

    fix 02-08-hm-js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -18,19 +18,25 @@ function onPlayTimeUpdate(data) {
 }
 
 function getCurrentPlayTime() {
+  let getStorage = localStorage.getItem(STORAGE_KEY);
+
+  if (!getStorage) {
+    return;
+  }
+
   try {
-    const getStorage = localStorage.getItem(STORAGE_KEY);
-    return getStorage;
+    return JSON.parse(getStorage);
   } catch ({ message, name }) {
     console.log(name);
     console.log(message);
   }
 }
-player.setCurrentTime(getCurrentPlayTime());
-// Ознайомся з документацією бібліотеки Vimeo плеєра.
-// Додай бібліотеку як залежність проекту через npm.
-// Ініціалізуй плеєр у файлі скрипта як це описано в секції pre-existing player, але враховуй, що у тебе плеєр доданий як npm пакет, а не через CDN.
-// Вивчи документацію методу on() і почни відстежувати подію timeupdate - оновлення часу відтворення.
-// Зберігай час відтворення у локальне сховище. Нехай ключем для сховища буде рядок "videoplayer-current-time".
-// Під час перезавантаження сторінки скористайся методом setCurrentTime() з метою відновлення відтворення зі збереженої позиції.
-// Додай до проекту бібліотеку lodash.throttle і зроби так, щоб час відтворення оновлювався у сховищі не частіше, ніж раз на секунду.
+
+player.setCurrentTime(getCurrentPlayTime()).catch(error => {
+  switch (error.name) {
+    case 'RangeError':
+      break;
+    default:
+      break;
+  }
+});
